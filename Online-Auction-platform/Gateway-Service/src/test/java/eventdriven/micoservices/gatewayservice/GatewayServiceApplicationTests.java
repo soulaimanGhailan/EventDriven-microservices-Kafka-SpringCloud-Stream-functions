9412b72f4896dee0a824c791899b0f94f5904e48
soulaiman ghailan <soulaiman8ghailan@gmail.com>
2023-11-07 23:44:26 +0100

    first commit

---
+++ Online-Auction-platform/Gateway-Service/src/test/java/eventdriven/micoservices/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eventdriven.micoservices.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
